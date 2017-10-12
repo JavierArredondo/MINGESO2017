0a906a1216fff78b5a16455c6f5f4cc83b614c45
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2017-10-12 16:28:01 -0300

    test

--- src/main/java/Grupo6_TMingueso/Tingeso/TingesoApplication.java
+++ src/main/java/Grupo6_TMingueso/Tingeso/TingesoApplication.java
@@ -2,8 +2,9 @@ package Grupo6_TMingueso.Tingeso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hola
 @SpringBootApplication
+
 public class TingesoApplication {
 
 	public static void main(String[] args) {
