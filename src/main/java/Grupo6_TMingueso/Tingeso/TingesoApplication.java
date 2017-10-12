2b769b751d0a34f65ebe25cd57dcc8b442a08f65
MarioCaceres <mario.caceres@usach.cl>
2017-10-12 16:00:32 -0300

    Initial commit

---
+++ src/main/java/Grupo6_TMingueso/Tingeso/TingesoApplication.java
@@ -0,0 +1,12 @@
+package Grupo6_TMingueso.Tingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TingesoApplication.class, args);
+	}
+}
