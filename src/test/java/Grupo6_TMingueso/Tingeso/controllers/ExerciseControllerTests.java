ec100a4033dcbb6d58992e39110b84431da7e55c
MarioCaceres <mario.caceres@usach.cl>
2017-10-25 18:55:31 -0300

    Exercises Test

---
+++ src/test/java/Grupo6_TMingueso/Tingeso/controllers/ExerciseControllerTests.java
@@ -0,0 +1,49 @@
+package Grupo6_TMingueso.Tingeso.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import static org.assertj.core.api.Assertions.*;
+import Grupo6_TMingueso.Tingeso.models.Exercise;
+import Grupo6_TMingueso.Tingeso.repository.ExerciseRepository;
+/**
+ * Created by mario on 25-10-17.
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ExerciseControllerTests {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ExerciseRepository Repo;
+
+
+    @Test
+    public void CreateExerciseTest(){
+        // Insert
+        Exercise test = new Exercise();
+        test.setTitle("New Exercise");
+        test.setText("Try and solve this very hard problem");
+        test.setAnswer("one");
+        entityManager.persist(test);
+        entityManager.flush();
+
+        // Search.
+        Exercise found = sRepo.findByName(test.getTitle());
+        System.out.println("+++++++++++TEST CREATE EXERCISE++++++++++++");
+        System.out.println("Title: "+found.getTitle());
+        System.out.println("Text: "+found.getText());
+        System.out.println("Answer: "+found.getText());
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+
+        // Assert
+        assertThat(found.getTitle())
+                .isEqualTo(test.getTitle());
+    }
+
+}
