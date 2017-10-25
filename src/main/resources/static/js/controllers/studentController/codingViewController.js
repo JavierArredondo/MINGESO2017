52959f4e0b18ee4eaa7213aa938ccb54de5da616
Dragomir Sekul <dragomir.sekul@usach.cl>
2017-10-25 11:48:35 -0300

    Coding view updated and change code editor theme

--- src/main/resources/static/js/controllers/studentController/codingViewController.js
+++ src/main/resources/static/js/controllers/studentController/codingViewController.js
@@ -1,9 +1,12 @@
 app.controller('codingViewController', ['$scope', '$http', function ($scope, $http) {
 
+    $scope.spinner = true;
+    $scope.results = false;
+
     $scope.myCodeMirrorOptions = {
         mode: "python",
         lineNumbers: true,
-        theme: "base16-light",
+        theme: "material",
         value: "Escriba aquí su código..."
     };
 
@@ -14,10 +17,20 @@ app.controller('codingViewController', ['$scope', '$http', function ($scope, $ht
         console.log(code);
         $http.post('/code', code).then(function (response) {
             console.log(response);
-            var status = response.status;
+            $scope.errors = response.data.error;
+            $scope.output = response.data.output;
+            $scope.status = response.data.statusCode;
             console.log(status);
+            $scope.spinner = false;
+            $scope.results = true;
         });
 
+    }
+
+    $scope.resetPopup = function () {
+        $scope.status = 3;
+        $scope.spinner = true;
+        $scope.results = false;
     }
 
 
