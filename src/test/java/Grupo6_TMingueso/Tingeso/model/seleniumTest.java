d19c2970520f37f13a570da7e0ea67944567bc0d
huasocristian <cristian.espinoza.s@usach.cl>
2017-10-26 09:56:34 -0300

    test ci

--- src/test/java/Grupo6_TMingueso/Tingeso/model/seleniumTest.java
+++ src/test/java/Grupo6_TMingueso/Tingeso/model/seleniumTest.java
@@ -19,18 +19,17 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class seleniumTest {
- //
-    /*private WebDriver driver;
-    private Capabilities chromeCapabilities = DesiredCapabilities.chrome();
+    private WebDriver driver;
+    private Capabilities chromeCapabilities = DesiredCapabilities.firefox();
 
     @Before
     public void setUp() throws Exception,MalformedURLException{
 
-        driver = new RemoteWebDriver(new URL("http://207.154.197.207:4444//wd/hub") , chromeCapabilities) ;
-        //DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        //capabilities.setBrowserName("chrome");
-        //capabilities.setPlatform(Platform.LINUX);
-        //driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), capabilities);
+        //driver = new RemoteWebDriver(new URL("http://207.154.197.207:4444//wd/hub") , chromeCapabilities) ;
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setBrowserName("chrome");
+        capabilities.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), capabilities);
         //driver = new FirefoxDriver();
         //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
@@ -363,5 +362,5 @@ public class seleniumTest {
     @After
     public void tearDown() throws Exception {
         this.driver.quit();
-    }*/
+    }
 }
