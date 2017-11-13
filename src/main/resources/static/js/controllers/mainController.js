1366a15df222dbf291da57759c598269b22ef87b
MarioCaceres <mario.caceres@usach.cl>
2017-11-13 03:11:20 -0300

    Login page rework and hide user mechanics

--- src/main/resources/static/js/controllers/mainController.js
+++ src/main/resources/static/js/controllers/mainController.js
@@ -1,4 +1,4 @@
-app.controller('mainController', ['$scope','$location', function($scope,$location) {
+app.controller('mainController', ['$scope','$location','userService', function($scope,$location,userService) {
 
 	$scope.title = "Home";
 
@@ -6,6 +6,27 @@ app.controller('mainController', ['$scope','$location', function($scope,$locatio
     $scope.home = $location.path() === '/home';
 
 	$location.path('/login');
+
+	$scope.session = false;
+
+	$scope.homerole = function(){
+		if(userService.getUser().role == 'student'){
+			$scope.home = '/student/home';
+		}
+		else if(userService.getUser().role == 'teacher'){
+			$scope.home = '/teacher/home';
+		}
+		else if(userService.getUser().role == 'coordinator'){
+			$scope.home = '/coordinator/home';
+		}
+		else if(userService.getUser().role == 'administrator'){
+			$scope.home = '/administrator/home';
+		}
+		else{
+			$scope.home = '/login';
+		}
+	};
+	$scope.homerole();
 
 	$scope.navigation = [
 	{"text":"Iniciar sesión",   "link":"#!/login", "condition": $scope.login , },
