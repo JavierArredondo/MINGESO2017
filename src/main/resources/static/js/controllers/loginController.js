58b3d6ba8aa680f2628e944bb8a28135af5719d6
MarioCaceres <mario.caceres@usach.cl>
2017-11-13 04:39:26 -0300

    Implemented login v1, missing db check and cross role security

--- src/main/resources/static/js/controllers/loginController.js
+++ src/main/resources/static/js/controllers/loginController.js
@@ -1,8 +1,11 @@
-app.controller('loginController', ['$scope','$location','$http', function($scope,$location,$http) {
+app.controller('loginController', ['$scope','$location','$http', 'GooglePlus','userService', function($scope,$location,$http,GooglePlus,userService) {
     $scope.title = "Inicio de sesión";
 
-        $scope.go = function ( path ) {
-          $location.path( path );
-        };
+    $scope.go = function ( path ) {
+         $location.path( path );
+    };
+
+
+
 
 }]);
